8abe8ef4fde4487670410c7278718916da7d1bb4
Gregory <greg.henselman@gmail.com>
2021-07-25 19:00:02 +0100

    Compiles!

--- src/utilities.rs
+++ src/utilities.rs
@@ -11,7 +11,7 @@
 
 
 /// Last value of a vector.
-pub fn end_val< T: Clone >( v: Vec<T> ) -> Option< T >  {
+pub fn end_val< T: Clone >( v: & Vec<T> ) -> Option< T >  {
     match v.is_empty() { 
         true    =>  None, 
         false   =>  Some( v[ v.len() - 1].clone() ) 
@@ -20,14 +20,14 @@ pub fn end_val< T: Clone >( v: Vec<T> ) -> Option< T >  {
 
 /// Mutable reference to last value of a vector.
 pub fn end_val_mut< 'a, T >( v: &'a mut Vec<T> ) -> Option< &'a mut T > {
-    match v.is_empty() { 
-        true    =>  None, 
-        false   =>  Some( &mut v[ v.len() - 1] ) 
+    match end_index(v) { 
+        None        =>  None, 
+        Some(i)   =>  Some( &mut v[i] ) 
     }
 }
 
 /// Last ordinal for a vector
-pub fn end_index< T > ( v: Vec<T> ) -> Option< usize > { 
+pub fn end_index< T > ( v: & Vec<T> ) -> Option< usize > { 
     match v.is_empty() { 
         true    =>  None, 
         false   =>  Some(v.len() - 1) 
